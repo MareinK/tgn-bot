c592cfbcf5f16ebebe74bf80f0c7a07915e39173
Marein <mail@marein.net>
2021-03-12 18:11:21 +0100

    Welcome new members and respond to mentions (fluff)

---
+++ src/tgn_bot/core.clj
@@ -0,0 +1,54 @@
+(ns tgn-bot.core
+  (:require [clojure.edn :as edn]
+            [clojure.core.async :refer [chan close!]]
+            [discljord.messaging :as discord-rest]
+            [discljord.connections :as discord-ws]
+            [discljord.formatting :refer [mention-user]]
+            [discljord.events :refer [message-pump!]]))
+
+(def state (atom nil))
+
+(def bot-id (atom nil))
+
+(def config (edn/read-string (slurp "config.edn")))
+
+(defmulti handle-event (fn [type _data] type))
+
+(defn random-response [user]
+  (str (rand-nth (:responses config)) ", " (mention-user user) \!))
+
+(defmethod handle-event :message-create
+  [_ {:keys [channel-id author mentions] :as _data}]
+  (when (some #{@bot-id} (map :id mentions))
+    (discord-rest/create-message! (:rest @state) channel-id :content (str (random-response author) " " channel-id))))
+
+(defn introduction-message [user]
+  (format (:introduction-message config) (mention-user user)))
+
+(defmethod handle-event :guild-member-add
+  [_ {:keys [user] :as _data}]
+  (println _data)
+  (discord-rest/create-message! (:rest @state) (:introduction-channel-id config) :content (introduction-message user)))
+
+(defmethod handle-event :default [type data]
+  (println type data))
+
+(defn start-bot! [token & intents]
+  (let [event-channel (chan 100)
+        gateway-connection (discord-ws/connect-bot! token event-channel :intents (set intents))
+        rest-connection (discord-rest/start-connection! token)]
+    {:events  event-channel
+     :gateway gateway-connection
+     :rest    rest-connection}))
+
+(defn stop-bot! [{:keys [rest gateway events] :as _state}]
+  (discord-rest/stop-connection! rest)
+  (discord-ws/disconnect-bot! gateway)
+  (close! events))
+
+(defn -main [& args]
+  (reset! state (start-bot! (slurp "token") :guild-members :guild-messages :direct-messages))
+  (reset! bot-id (:id @(discord-rest/get-current-user! (:rest @state))))
+  (try
+    (message-pump! (:events @state) handle-event)
+    (finally (stop-bot! @state))))
